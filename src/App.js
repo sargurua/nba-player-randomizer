050891653a2e082caf789fcb32a07bfcfb8d8886
Avinash Sarguru <avinashsarguru@gmail.com>
2020-01-14 12:30:43 -0600

    header and icon/

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Menu from './components/Menu'
+import {Header, Icon} from 'semantic-ui-react'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header as='h2' icon>
+        <Icon name='basketball ball' color='orange'/>
+        NBA Player Randomizer
+      </Header>
+      <Menu />
     </div>
   );
 }
