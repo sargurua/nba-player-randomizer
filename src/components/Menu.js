2904fb25ceb044cb989036ad9d8d9bfeb3d92b46
Avinash Sarguru <avinashsarguru@gmail.com>
2020-01-15 10:53:40 -0600

    added dropdown and button

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,11 +1,15 @@
 import React, {Component} from 'react'
-
+import NumDropdown from './Dropdown'
 
 export default class Menu extends Component {
+    onSubmit = event => {
+        console.log(event)
+    }
+
     render() {
         return (
             <div>
-
+                <NumDropdown onSubmit={this.onSubmit}/>
             </div>
         )
         
