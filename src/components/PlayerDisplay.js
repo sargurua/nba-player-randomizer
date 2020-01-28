a9354edc5a52a7fc9c4b2cfa0a2def381d419286
Avinash Sarguru <avinashsarguru@gmail.com>
2020-01-28 11:18:47 -0600

    Grouping of card outputs, need to fix grouping overlayed ontop of button

--- src/components/PlayerDisplay.js
+++ src/components/PlayerDisplay.js
@@ -2,18 +2,25 @@ import React, {Component} from 'react'
 
 import PlayerCard from './PlayerCard'
 
-import { Button } from 'semantic-ui-react'
+import { Button, Grid, Card } from 'semantic-ui-react'
 
 export default class PlayerDisplay extends Component {
 
     renderDisplay = () => {
-        return this.props.players.map(player => {
-            return (
-                <div key={player.name}>
-                    <PlayerCard player={player} />
-                </div>
-            )
-        })
+        let i = 0
+        
+        return(
+            <Card.Group itemsPerRow={5} centered>
+                {this.props.players.map(player => {
+                    return (
+                        <div key={player.name}>
+                            <PlayerCard player={player} />
+                        </div>
+                        
+                    )
+                })}
+            </Card.Group>
+        )
     }
 
     render() {
