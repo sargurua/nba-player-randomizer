d7c6e288192d47e349992c23eded417160a600b1
Avinash Sarguru <avinashsarguru@gmail.com>
2020-01-28 11:07:29 -0600

    Card styling

--- src/components/PlayerCard.js
+++ src/components/PlayerCard.js
@@ -6,7 +6,10 @@ export default ({player}) => {
     return (
         <Card fluid>
             <Image centered src={`https://ak-static.cms.nba.com/wp-content/uploads/headshots/nba/${player.team_ref}/2019/260x190/${player.player_ref}.png`} onError={e => e.target.src=`https://ak-static.cms.nba.com/wp-content/uploads/headshots/nba/latest/260x190/${player.player_ref}.png`} alt={`${player.first_name} ${player.last_name}`} />
-            <Header as='h2'>{player.name}</Header>
+            <Header dividing>
+                {player.name} - {player.position.replace("-", "/")}
+                <Header.Subheader>{player.team}</Header.Subheader>
+            </Header>
         </Card>
     )
 }
